48bf53d64e57beaca2c1db0ee900befad21cf875
Zerohertz <ohg3417@gmail.com>
2023-07-04 11:18:40 +0900

    :art: Upadate: Visualization of GT

--- datasets/batch_visualization.py
+++ datasets/batch_visualization.py
@@ -2,11 +2,14 @@ import os
 import shutil
 import random
 import numpy as np
+import torch
 import cv2
+
+from util.visualize import vis_output_seqs
 
 
 class BatchVisualization:
-    def __init__(self, epoch, root='bv', dir='bv'):
+    def __init__(self, epoch, root='bv', dir='bv', ):
         self.dir = os.path.join(root, dir + '_' + str(epoch))
         self.epoch = epoch
         try:
@@ -16,13 +19,15 @@ class BatchVisualization:
         if not root in os.listdir():
             os.mkdir(root)
         os.mkdir(self.dir)
-    def bv(self, samples): #, ibs, ils, obs, ols):
-        # TODO: Visualization of Ground Truth
-        for sample in samples:
-            img = sample.clone().numpy()
-            img = np.transpose(img, (1,2,0))
-            img *= 255
-            img = img.astype(np.uint8)
+        with open('data/dict.txt', 'r') as f:
+            self.chars = f.readlines()[0]
+    def bv(self, samples, ils):
+        '''
+        FIXME: Sometimes, there are wrong GTs in results
+        '''
+        gt_scores = torch.ones(1, 5000, 1000 + len(self.chars))
+        imgs = vis_output_seqs(samples, ils, gt_scores, remove_padding=False, pad_rec=True, text_length=50, chars=self.chars)
+        for img in imgs:
             name = os.path.join(
                 self.dir,
                 str(random.randrange(100_000, 999_999)) + '.jpg'
